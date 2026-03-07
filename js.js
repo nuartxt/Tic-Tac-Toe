e2c8ab8c54db758eda0ea14298c3dbac1d62e725
Anuar <151063105+nuartxt@users.noreply.github.com>
2026-03-07 21:51:21 +0500

    fix bag with "finding winner by cross"

--- js.js
+++ js.js
@@ -5,6 +5,9 @@ let theInputField = [
 ];
 
 
+
+
+
 let stepPlayer1 = [];
 let stepPlayer2 = [];
 
@@ -13,7 +16,7 @@ let stepPlayer2 = [];
 function callPrompt() {
     for (let i = 0; i < 5; i++) {
         let player1_index = Number(prompt("Go X"));
-        let player2_index = Number(prompt("Go Y"));
+        let player2_index = Number(prompt("Go 0"));
 
         stepPlayer1.push(player1_index);
         stepPlayer2.push(player2_index);
@@ -32,58 +35,83 @@ function addInGameBox(bigArray) {
 }
 
 
-callPrompt();
-addInGameBox(theInputField);
+// callPrompt();
+// addInGameBox(theInputField);
 
 
 
 
-/* */
+// /* by cross */
 
-let result = ""
+// let result = ""
 
-for (let i = 0; i < theInputField.length; i += 4) {
-    if (theInputField[i] == "X") {
-        result = "X win";
-    } else if (theInputField[i] == "0") {
-        result = "0 win";
-    }
+// let leftCrossArr = [];
+// for (let i = 0; i < theInputField.length; i += 4) {
+//     leftCrossArr.push(theInputField[i]);
+//     if (leftCrossArr.length == 3 && leftCrossArr.every(el => el === "X")) {
+//         result = "X win";
+//     } else if (leftCrossArr.length == 3 && leftCrossArr.every(el => el === "0")) {
+//         result = "0 win";
+//     }
+// }
+// console.log(leftCrossArr)
+
+// let rightCrossArr = [];
+// for (let i = 2; i < theInputField.length - 1; i += 2) {
+//     rightCrossArr.push(theInputField[i]);
+//     if (rightCrossArr.length == 3 && rightCrossArr.every(el => el == "X")) {
+//         result = "X win";
+//     }
+//     if (rightCrossArr.length == 3 && rightCrossArr.every(el => el == "0")) {
+//         result = "0 win";
+//     }
+// }
+// console.log(rightCrossArr);
+
+// /* by line */
+
+// let a = theInputField[0];
+
+// for (let i = 0; i < theInputField.length; i++) {
+//     if (a == "X" && a + 1 == "X" && a + 2 == "X") {
+//         result = "X win";
+//     } else if (a == "0" && a + 1 == "0" && a + 2 == "0") {
+//         result = "0 win";
+//     }
+//     if (a + 3 == "X" && a + 4 == "X" && a + 5 == "X") {
+//         result = "X win";
+//     } else if (a == "0" && a + 1 == "0" && a + 2 == "0") {
+//         result = "0 win";
+//     }
+//     if (a + 6 == "X" && a + 7 == "X" && a + 8 == "X") {
+//         result = "X win";
+//     } else if (a == "0" && a + 1 == "0" && a + 2 == "0") {
+//         result = "0 win";
+//     }
+// }
+
+/*by column */
+let column = 1;
+for(let i = 0; i < 3; i++){
+    column ++;
 }
-
-for (let i = 2; i < theInputField.length - 1; i += 2) {
-    if (theInputField[i] == "X") {
-        result = "X win";
-    } else if (theInputField[i] == "0") {
-        result = "0 win";
-    }
+let columnArr = [];
+// for (let i = column; i < theInputField.length; i += 3) {
+//     columnArr.push(theInputField[i]);
+//     if (columnArr.length == 3 && columnArr.every(el => el == "X")) {
+//         result = "X win";
+//     } else if (columnArr.length == 3 && columnArr.every(el => el == "0")) {
+//         result = "0 win";
+//     } else {
+//         column++;
+//     }
+// }
+for (let i = 0; i < theInputField.length; i += 3) {
+    columnArr.push(theInputField[i]);
 }
-
-
-let a = theInputField[0];
-
-
-for (let i = 0; i < theInputField.length; i++) {
-    if (a == "X" && a + 1 == "X" && a + 2 == "X") {
-        result = "X win";
-    } else if (a == "0" && a + 1 == "0" && a + 2 == "0")  {
-        result = "0 win";
-    }
-    if (a + 3 == "X" && a + 4 == "X" && a + 5 == "X") {
-        result = "X win";
-    } else if (a == "0" && a + 1 == "0" && a + 2 == "0") {
-        result = "0 win";
-    }
-    if (a + 6 == "X" && a + 7 == "X" && a + 8 == "X") {
-        result = "X win";
-    } else if (a == "0" && a + 1 == "0" && a + 2 == "0") {
-        result = "0 win";
-    }
-}
-
-
+console.log(columnArr);
 
 
 
 console.log(theInputField);
-
 console.log(result);
